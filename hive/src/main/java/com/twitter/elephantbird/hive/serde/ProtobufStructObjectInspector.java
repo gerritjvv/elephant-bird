c95f54fec9cf65f0a0468a6a478137f1282d4da6
gerritjvv <gerritjvv@gmail.com>
2013-12-30 19:24:03 +0100

    convert to java types

--- hive/src/main/java/com/twitter/elephantbird/hive/serde/ProtobufStructObjectInspector.java
+++ hive/src/main/java/com/twitter/elephantbird/hive/serde/ProtobufStructObjectInspector.java
@@ -1,5 +1,7 @@
 package com.twitter.elephantbird.hive.serde;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -12,6 +14,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.StructField;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 
 import com.google.common.collect.Lists;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.EnumValueDescriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
@@ -19,175 +22,193 @@ import com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
 import com.google.protobuf.Descriptors.FieldDescriptor.Type;
 import com.google.protobuf.Message;
 
-public final class ProtobufStructObjectInspector extends SettableStructObjectInspector {
+public final class ProtobufStructObjectInspector extends
+		SettableStructObjectInspector {
 
-  public static class ProtobufStructField implements StructField {
+	public static class ProtobufStructField implements StructField {
 
-    private ObjectInspector oi = null;
-    private String comment = null;
-    private FieldDescriptor fieldDescriptor;
+		private ObjectInspector oi = null;
+		private String comment = null;
+		private FieldDescriptor fieldDescriptor;
 
-    @SuppressWarnings("unchecked")
-    public ProtobufStructField(FieldDescriptor fieldDescriptor) {
-      this.fieldDescriptor = fieldDescriptor;
-      oi = this.createOIForField();
-    }
+		@SuppressWarnings("unchecked")
+		public ProtobufStructField(FieldDescriptor fieldDescriptor) {
+			this.fieldDescriptor = fieldDescriptor;
+			oi = this.createOIForField();
+		}
 
-    @Override
-    public String getFieldName() {
-      return fieldDescriptor.getName();
-    }
+		@Override
+		public String getFieldName() {
+			return fieldDescriptor.getName();
+		}
 
-    @Override
-    public ObjectInspector getFieldObjectInspector() {
-      return oi;
-    }
+		@Override
+		public ObjectInspector getFieldObjectInspector() {
+			return oi;
+		}
 
-    @Override
-    public String getFieldComment() {
-      return comment;
-    }
+		@Override
+		public String getFieldComment() {
+			return comment;
+		}
 
-    public FieldDescriptor getFieldDescriptor() {
-      return fieldDescriptor;
-    }
+		public FieldDescriptor getFieldDescriptor() {
+			return fieldDescriptor;
+		}
 
-    private PrimitiveCategory getPrimitiveCategory(JavaType fieldType) {
-      switch (fieldType) {
-        case INT:
-          return PrimitiveCategory.INT;
-        case LONG:
-          return PrimitiveCategory.LONG;
-        case FLOAT:
-          return PrimitiveCategory.FLOAT;
-        case DOUBLE:
-          return PrimitiveCategory.DOUBLE;
-        case BOOLEAN:
-          return PrimitiveCategory.BOOLEAN;
-        case STRING:
-          return PrimitiveCategory.STRING;
-        case BYTE_STRING:
-          return PrimitiveCategory.BINARY;
-        case ENUM:
-          return PrimitiveCategory.STRING;
-        default:
-          return null;
-      }
-    }
+		private PrimitiveCategory getPrimitiveCategory(JavaType fieldType) {
+			switch (fieldType) {
+			case INT:
+				return PrimitiveCategory.INT;
+			case LONG:
+				return PrimitiveCategory.LONG;
+			case FLOAT:
+				return PrimitiveCategory.FLOAT;
+			case DOUBLE:
+				return PrimitiveCategory.DOUBLE;
+			case BOOLEAN:
+				return PrimitiveCategory.BOOLEAN;
+			case STRING:
+				return PrimitiveCategory.STRING;
+			case BYTE_STRING:
+				return PrimitiveCategory.BINARY;
+			case ENUM:
+				return PrimitiveCategory.STRING;
+			default:
+				return null;
+			}
+		}
 
-    private ObjectInspector createOIForField() {
-      JavaType fieldType = fieldDescriptor.getJavaType();
-      PrimitiveCategory category = getPrimitiveCategory(fieldType);
-      ObjectInspector elementOI = null;
-      if (category != null) {
-        elementOI = PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(category);
-      } else {
-        switch (fieldType) {
-          case MESSAGE:
-            elementOI = new ProtobufStructObjectInspector(fieldDescriptor.getMessageType());
-            break;
-          default:
-            throw new RuntimeException("JavaType " + fieldType
-                + " from protobuf is not supported.");
-        }
-      }
-      if (fieldDescriptor.isRepeated()) {
-        return ObjectInspectorFactory.getStandardListObjectInspector(elementOI);
-      } else {
-        return elementOI;
-      }
-    }
-  }
+		private ObjectInspector createOIForField() {
+			JavaType fieldType = fieldDescriptor.getJavaType();
+			PrimitiveCategory category = getPrimitiveCategory(fieldType);
+			ObjectInspector elementOI = null;
+			if (category != null) {
+				elementOI = PrimitiveObjectInspectorFactory
+						.getPrimitiveJavaObjectInspector(category);
+			} else {
+				switch (fieldType) {
+				case MESSAGE:
+					elementOI = new ProtobufStructObjectInspector(
+							fieldDescriptor.getMessageType());
+					break;
+				default:
+					throw new RuntimeException("JavaType " + fieldType
+							+ " from protobuf is not supported.");
+				}
+			}
+			if (fieldDescriptor.isRepeated()) {
+				return ObjectInspectorFactory
+						.getStandardListObjectInspector(elementOI);
+			} else {
+				return elementOI;
+			}
+		}
+	}
 
-  private Descriptor descriptor;
-  private List<StructField> structFields = Lists.newArrayList();
+	private Descriptor descriptor;
+	private List<StructField> structFields = Lists.newArrayList();
 
-  ProtobufStructObjectInspector(Descriptor descriptor) {
-    this.descriptor = descriptor;
-    for (FieldDescriptor fd : descriptor.getFields()) {
-      structFields.add(new ProtobufStructField(fd));
-    }
-  }
+	ProtobufStructObjectInspector(Descriptor descriptor) {
+		this.descriptor = descriptor;
+		for (FieldDescriptor fd : descriptor.getFields()) {
+			structFields.add(new ProtobufStructField(fd));
+		}
+	}
 
-  @Override
-  public Category getCategory() {
-    return Category.STRUCT;
-  }
+	@Override
+	public Category getCategory() {
+		return Category.STRUCT;
+	}
 
-  @Override
-  public String getTypeName() {
-    StringBuilder sb = new StringBuilder("struct<");
-    boolean first = true;
-    for (StructField structField : getAllStructFieldRefs()) {
-      if (first) {
-        first = false;
-      } else {
-        sb.append(",");
-      }
-      sb.append(structField.getFieldName()).append(":")
-          .append(structField.getFieldObjectInspector().getTypeName());
-    }
-    sb.append(">");
-    return sb.toString();
-  }
+	@Override
+	public String getTypeName() {
+		StringBuilder sb = new StringBuilder("struct<");
+		boolean first = true;
+		for (StructField structField : getAllStructFieldRefs()) {
+			if (first) {
+				first = false;
+			} else {
+				sb.append(",");
+			}
+			sb.append(structField.getFieldName())
+					.append(":")
+					.append(structField.getFieldObjectInspector().getTypeName());
+		}
+		sb.append(">");
+		return sb.toString();
+	}
 
-  @Override
-  public Object create() {
-    return descriptor.toProto().toBuilder().build();
-  }
+	@Override
+	public Object create() {
+		return descriptor.toProto().toBuilder().build();
+	}
 
-  @Override
-  public Object setStructFieldData(Object data, StructField field, Object fieldValue) {
-    return ((Message) data)
-        .toBuilder()
-        .setField(descriptor.findFieldByName(field.getFieldName()), fieldValue)
-        .build();
-  }
+	@Override
+	public Object setStructFieldData(Object data, StructField field,
+			Object fieldValue) {
+		return ((Message) data)
+				.toBuilder()
+				.setField(descriptor.findFieldByName(field.getFieldName()),
+						fieldValue).build();
+	}
 
-  @Override
-  public List<? extends StructField> getAllStructFieldRefs() {
-    return structFields;
-  }
+	@Override
+	public List<? extends StructField> getAllStructFieldRefs() {
+		return structFields;
+	}
 
-  @Override
-  public Object getStructFieldData(Object data, StructField structField) {
-    if (data == null) {
-      return null;
-    }
-    Message m = (Message) data;
-    ProtobufStructField psf = (ProtobufStructField) structField;
-    FieldDescriptor fieldDescriptor = psf.getFieldDescriptor();
-    Object result = m.getField(fieldDescriptor);
-    if (fieldDescriptor.getType() == Type.ENUM) {
-     if(result instanceof List)
-    	 return ((List)result).get(fieldDescriptor.getIndex());
-     else if(result instanceof Collection){
-    	 Iterator it = ((Collection)result).iterator();
-    	 for(int i = 0; i < fieldDescriptor.getIndex(); i++)
-    			 it.next();
-    	 
-    	 return it.hasNext() ? it.next() : null;
-     }else
-    	return ((EnumValueDescriptor)result).getName();
-    }
-    return result;
-  }
+	@Override
+	public Object getStructFieldData(Object data, StructField structField) {
+		if (data == null) {
+			return null;
+		}
+		Message m = (Message) data;
+		ProtobufStructField psf = (ProtobufStructField) structField;
+		FieldDescriptor fieldDescriptor = psf.getFieldDescriptor();
+		Object result = m.getField(fieldDescriptor);
 
-  @Override
-  public StructField getStructFieldRef(String fieldName) {
-    return new ProtobufStructField(descriptor.findFieldByName(fieldName));
-  }
+		return toJava(fieldDescriptor, result);
+	}
 
-  @Override
-  public List<Object> getStructFieldsDataAsList(Object data) {
-    if (data == null) {
-      return null;
-    }
-    List<Object> result = Lists.newArrayList();
-    Message m = (Message) data;
-    for (FieldDescriptor fd : descriptor.getFields()) {
-      result.add(m.getField(fd));
-    }
-    return result;
-  }
+	private Object toJava(FieldDescriptor fieldDescriptor, Object result) {
+
+		if (result instanceof Collection) {
+			System.out
+					.println("Got field descriptor enum: but the data is a collection: "
+							+ Arrays.toString(((Collection) result).toArray()));
+
+			final Collection coll = (Collection) result;
+			final List l = new ArrayList();
+			final Iterator it = coll.iterator();
+
+			while (it.hasNext())
+				l.add(toJava(fieldDescriptor, it.next()));
+
+			return result;
+		} else if (result instanceof EnumValueDescriptor) {
+			return ((EnumValueDescriptor) result).getName();
+		} else if (result instanceof ByteString) {
+			return ((ByteString) result).toStringUtf8();
+		}else
+			return null;
+	}
+
+	@Override
+	public StructField getStructFieldRef(String fieldName) {
+		return new ProtobufStructField(descriptor.findFieldByName(fieldName));
+	}
+
+	@Override
+	public List<Object> getStructFieldsDataAsList(Object data) {
+		if (data == null) {
+			return null;
+		}
+		List<Object> result = Lists.newArrayList();
+		Message m = (Message) data;
+		for (FieldDescriptor fd : descriptor.getFields()) {
+			result.add(m.getField(fd));
+		}
+		return result;
+	}
 }
